2bf35a256420beec096cc00371959cdad1edc69b
Camilo Garcia <hernancgarcia3@gmail.com>
2025-11-27 12:48:20 -0500

    [+]Se resuelve el 5to ejercicio.

---
+++ Taller-1-basico/05.js
@@ -0,0 +1,8 @@
+//Escribe un programa que le pida al usuario ingresar una frase. El programa debe imprimir la frase en la consola 10 veces. NOTA: utiliza un ciclo para imprimir la frase las 10 veces.
+
+let frase = prompt( 'Ingresa una frase.');
+
+for (let i = 0; i < 10; i++){               //Si sale bien pero no veo completa la lista de frases en la consola, solo el número de veces y la frase.
+    console.log( frase );
+}
+
